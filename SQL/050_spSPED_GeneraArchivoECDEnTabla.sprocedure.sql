04ecefc738abeeebca0f68edb81103084d924b0e
jc fernandezz <juan-carlos.fernandez@tiiselam.com>
2020-03-18 18:58:06 -0400

    Agrega layout 8.00 para sped ecd del año 2019

--- SQL/050_spSPED_GeneraArchivoECDEnTabla.sprocedure.sql
+++ SQL/050_spSPED_GeneraArchivoECDEnTabla.sprocedure.sql
@@ -1,29 +1,47 @@
-USE [GBRA]
+IF EXISTS (
+  SELECT * 
+    FROM INFORMATION_SCHEMA.ROUTINES 
+   WHERE SPECIFIC_SCHEMA = 'dbo'
+     AND SPECIFIC_NAME = 'SPED_GeneraArchivoECDEnTabla' 
+)
+   DROP PROCEDURE dbo.SPED_GeneraArchivoECDEnTabla;
 GO
+
 -- =========================================================================================================================
 -- Propósito. Llama al stored procedure que genera los datos del SPED en la tabla spedtbl9000. El layout corresponde al año.
 -- Requisito. -
---24/6/19 jcf Creación
+--24/06/19 jcf Creación
+--18/03/20 jcf Agrega layout 800
 -- =========================================================================================================================
 
-create PROCEDURE [dbo].SPED_GeneraArchivoECDEnTabla 
-	@IdCompañia varchar (8),
+
+CREATE PROCEDURE dbo.SPED_GeneraArchivoECDEnTabla
+	@IdCompania varchar (8),
 	@FechaDesde varchar(10),
 	@FechaHasta varchar(10)
 AS
 BEGIN
-
-	DECLARE @layout varchar(10)
+	DECLARE @layout varchar(10);
 	select @layout = parametros.param1
-	from dbo.fSpedParametros('LAYOUT'+convert(varchar(4), YEAR(@FechaHasta)), 'na', 'na', 'na', 'na', 'na', 'SPED') parametros
+	from dbo.fSpedParametros('LAYOUT'+convert(varchar(4), YEAR(@FechaHasta)), 'na', 'na', 'na', 'na', 'na', 'SPED') parametros;
 
 	if (@layout = '600')		--ecd layout v600
-		exec [dbo].[SPED_ArchivoTXT_l600] @IdCompañia, @FechaDesde, @FechaHasta; 
-	else 
-		INSERT INTO spedtbl9000 (LINEA,seccion, datos) 
-				values(0, 'err', 'Verifique los parámetros SPED en la configuración de compañía: ' + @layout);
-End
+		exec [dbo].[SPED_ArchivoTXT_l600] @IdCompania, @FechaDesde, @FechaHasta; 
+	else if (@layout = '800')		--ecd layout v800
+		exec [dbo].[SPED_ArchivoTXT_l800] @IdCompania, @FechaDesde, @FechaHasta; 
+	else
+		begin 
+			delete from spedtbl9000;
+			INSERT INTO spedtbl9000 (LINEA,seccion, datos) 
+					values(0, 'err', 'Verifique la versión de layout SPED en la configuración de compañía: ' + @layout + ' para el año: '+ convert(varchar(4), YEAR(@FechaHasta)));
+		end
+END
 go
 
 grant execute on [dbo].SPED_GeneraArchivoECDEnTabla to DYNGRP;
 GO
+---------------------------------------------------------------------------------------------------------------------------------
+IF (@@Error = 0) PRINT 'Creación exitosa de: SPED_GeneraArchivoECDEnTabla'
+ELSE PRINT 'Error en la creación de: SPED_GeneraArchivoECDEnTabla'
+GO
+
